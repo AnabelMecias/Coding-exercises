25d0cbea38769208dcffe858577ab7bee5eab807
AnabelMecias <anabel.mecias19981111@gmail.com>
2023-07-13 21:24:01 -0400

    Consonants

--- src/App.java
+++ src/App.java
@@ -4,12 +4,24 @@ public class App {
         String sentence = "Anabel";
         System.out.println("The amount of vowels in "+sentence+" is: "+Vowels.countVowels(sentence));
 
+        System.out.println("-----------------------------------------");
+
         // Print whether the phrase is a palindrome or not.
         String phrase = "anay";
         System.out.println("Is the phrase "+phrase+" a palindrome?: "+Palindrome.isPalindrome(phrase));
 
+        System.out.println("-----------------------------------------");
+
         // Print the count of words in the given sentence.
         String sentenceTwo = "Believe in yourself and all that you are capable of achieving.";
-        System.out.println("The amount of Words in \"Believe in yourself and all that you are capable of achieving.\" is: "+Words.countWords(sentenceTwo));
+        System.out.println("The amount of Words in \""+sentenceTwo+"\" is: "+Words.countWords(sentenceTwo));
+
+        System.out.println("-----------------------------------------");
+
+        String sentenceThree = "Keep going, even when it gets tough.";
+        // Print the count of consonants in the given sentence.
+        System.out.println("The amount of consonants in \""+sentenceThree+"\" is: "+Consonants.countConsonants(sentenceThree));
+        // Print the new sentence with replaced consonants.
+        System.out.println("The new sentence is: "+Consonants.replaceConsonants(sentenceThree));
     }
 }
---
+++ src/Consonants.java
@@ -0,0 +1,24 @@
+public class Consonants {
+    public static int countConsonants (String sentence){
+        if(sentence.length() == 0) return 0;
+        String vowels = "aeiou";
+        int countConsonants = 0;
+        for (char ch : sentence.toCharArray()) {
+            if (Character.isLetter(ch) && vowels.indexOf(ch) == -1) {
+                countConsonants++;
+            }
+        }
+        return countConsonants;
+    }
+
+    public static String replaceConsonants (String sentence){
+        String vowels = "aeiou";
+        char[] chars = sentence.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (Character.isLetter(chars[i]) && vowels.indexOf(chars[i]) == -1) {
+                chars[i] = '*';
+            }
+        }
+        return new String(chars);
+    }
+}
