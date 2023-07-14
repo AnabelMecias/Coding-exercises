c9e3f1446e4a4794bd72150433c63acd967a76c7
AnabelMecias <anabel.mecias19981111@gmail.com>
2023-07-13 20:58:51 -0400

    Palindrome exercise

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        // Print the sentence along with the count of vowels in it.
         String sentence = "Anabel";
         System.out.println("The amount of vowels in "+sentence+" is: "+Vowels.countVowels(sentence));
+
+        String phrase = "anay";
+        System.out.println("Is the phrase "+phrase+" a palindrome?: "+Palindrome.isPalindrome(phrase));
+
     }
 }
---
+++ src/Palindrome.java
@@ -0,0 +1,6 @@
+public class Palindrome {
+    public static boolean isPalindrome(String phrase){
+        StringBuilder sBuilder = new StringBuilder(phrase.toLowerCase());
+        return (sBuilder.reverse().toString().equals(phrase.toLowerCase()));
+    }
+}
