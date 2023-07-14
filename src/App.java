cf9381aaf3ef210134916847f189e702bb9518ee
AnabelMecias <anabel.mecias19981111@gmail.com>
2023-07-13 20:52:12 -0400

    Counting vowels

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String sentence = "Anabel";
+        System.out.println("The amount of vowels in "+sentence+" is: "+Vowels.countVowels(sentence));
     }
 }
--- src/Vowels.java
+++ src/Vowels.java
@@ -1,5 +1,18 @@
 public class Vowels {
-    private String sentence;
+    public static int countVowels(String sentence)
+    {
+        int countVowels = 0;
+        if (sentence.length() == 0) {
+            return countVowels;
+        }
+        String vowels = "aeiou";
+        sentence = sentence.toLowerCase();
+        for (Character character : sentence.toCharArray()) {
+            if(vowels.indexOf(character) != -1){
+                countVowels++;
+            }
+        }
+        return countVowels;
+    }
 
-    
 }
