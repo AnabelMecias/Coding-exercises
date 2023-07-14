d294f9b40b6badcf0b70b727f767583f60331c71
AnabelMecias <anabel.mecias19981111@gmail.com>
2023-07-13 21:06:33 -0400

    Count words

--- src/App.java
+++ src/App.java
@@ -4,8 +4,12 @@ public class App {
         String sentence = "Anabel";
         System.out.println("The amount of vowels in "+sentence+" is: "+Vowels.countVowels(sentence));
 
+        // Print whether the phrase is a palindrome or not.
         String phrase = "anay";
         System.out.println("Is the phrase "+phrase+" a palindrome?: "+Palindrome.isPalindrome(phrase));
 
+        // Print the count of words in the given sentence.
+        String sentenceTwo = "Believe in yourself and all that you are capable of achieving.";
+        System.out.println("The amount of Words in \"Believe in yourself and all that you are capable of achieving.\" is: "+Words.countWords(sentenceTwo));
     }
 }
---
+++ src/Words.java
@@ -0,0 +1,6 @@
+public class Words {
+    public static int countWords(String sentence){
+        int amountWords = sentence.split(" ").length;
+        return amountWords;
+    }
+}
